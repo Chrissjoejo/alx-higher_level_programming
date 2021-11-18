be9eb28548bf662ea9ccf355234d0be93ec7269c
Chrissjoejo <Chrissjoejobrown112@gmail.com>
2021-11-18 12:18:30 +0000

    4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from hidden_4 import *
-    ar = dir()
-    for i in range(0, len(ar)):
-        if ar[i][0:2] != "__":
-            print("{}".format(ar[i]))
-
-
+    import hidden_4
+    for name in dir(hidden_4):
+        if name[0] != '_' and name[1] != '_':
+            print(name)
